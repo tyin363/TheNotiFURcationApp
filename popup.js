dd779eae99a754ec8f894cf6dcd034144df024b5
Tony <tyin363@aucklanduni.ac.nz>
2022-07-09 11:52:41 +1200

    Edited button

--- popup.js
+++ popup.js
@@ -1,7 +1,19 @@
-const notificationElement = document.getElementById("btn")
+const notificationElement = document.getElementById("btn");
 
 notificationElement.addEventListener("click", () => {
-    chrome.runtime.sendMessage({ time: "1" }, function (response) {
-        console.log(response);
-    })
+  chrome.runtime.sendMessage({ time: "1" }, function (response) {
+    console.log(response);
+  });
 });
+
+function toggle(el) {
+  if (el.className != "pause") {
+    el.src = "icon-hd-inverted.png";
+    el.className = "pause";
+  } else if (el.className == "pause") {
+    el.src = "icon-hd.png";
+    el.className = "play";
+  }
+
+  return false;
+}
