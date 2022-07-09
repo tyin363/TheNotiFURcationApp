3e1c82bbaf980327572a058ae182ba91c3962a01
Tony <tyin363@aucklanduni.ac.nz>
2022-07-09 12:17:25 +1200

    Edited popup

--- popup.js
+++ popup.js
@@ -5,15 +5,3 @@ notificationElement.addEventListener("click", () => {
     console.log(response);
   });
 });
-
-function toggle(el) {
-  if (el.className != "pause") {
-    el.src = "icon-hd-inverted.png";
-    el.className = "pause";
-  } else if (el.className == "pause") {
-    el.src = "icon-hd.png";
-    el.className = "play";
-  }
-
-  return false;
-}
