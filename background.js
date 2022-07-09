5b4f0aa8d581243692b2a13611bbf2e6bf3c90fd
Tony <tyin363@aucklanduni.ac.nz>
2022-07-09 16:17:35 +1200

    Edit

--- background.js
+++ background.js
@@ -1,69 +1,57 @@
-var random = Math.floor(Math.random()* 100) +10;
-
+var random = Math.floor(Math.random() * 100) + 10;
 
 let count = 2;
 
-async function fetchImage(){
+async function fetchImage() {
+  //let parentdiv = document.createElement('div')
+  //parentdiv.id = 'animal'
+  return fetch("https://www.reddit.com/r/Eyebleach.json?limit=10")
+    .then((response) => response.json())
+    .then((body) => {
+      for (let index = 2; index < body.data.children.length; index++) {
+        if (body.data.children[index].data.post_hint == "image") {
+          let image = body.data.children[index].data.url_overridden_by_dest;
+          console.log(typeof image);
+          console.log(typeof "icon-128.png");
+          console.log(image);
+          return image;
 
-    //let parentdiv = document.createElement('div')
-    //parentdiv.id = 'animal'
-    return fetch('https://www.reddit.com/r/Eyebleach.json?limit=10')
-    .then(response => response.json())
-    .then(body => {
-        for(let index = 2; index<body.data.children.length; index++){
-            if(body.data.children[index].data.post_hint=='image'){
-                let image = body.data.children[index].data.url_overridden_by_dest
-                console.log(typeof(image));
-                console.log(typeof('icon-128.png'))
-                console.log(image)
-                return(image)
-                
-
-                
-                //let div=document.createElement('div')
-                //let h4=document.createElement('h4')
-                //let image=doc ument.createElement('img')
-                //image.src=body.data.children[index].data.url_overridden_by_dest
-                //h4.textContent=body.data.children[index].data.title
-                //div.appendChild(h4)
-                //div.appendChild(image)
-                //parentdiv.appendChild(div)
-            }
+          //let div=document.createElement('div')
+          //let h4=document.createElement('h4')
+          //let image=doc ument.createElement('img')
+          //image.src=body.data.children[index].data.url_overridden_by_dest
+          //h4.textContent=body.data.children[index].data.title
+          //div.appendChild(h4)
+          //div.appendChild(image)
+          //parentdiv.appendChild(div)
         }
-    //document.body.appendChild(parentdiv)
+      }
+      //document.body.appendChild(parentdiv)
     });
 }
 
-chrome.alarms.onAlarm.addListener(
-    async () => {
-        chrome.notifications.create(
-            {
-            type: 'image',
-            iconUrl: 'icon-128.png',
-            imageUrl: await fetchImage(),
-            title: 'notification',
-            message: 'notification message',
-            priority: 2,
-            silent: false
-        },
-        () => {}
-        )
+chrome.alarms.onAlarm.addListener(async () => {
+  chrome.notifications.create(
+    {
+      type: "image",
+      iconUrl: "icon-128.png",
+      imageUrl: await fetchImage(),
+      title: "notification",
+      message: "notification message",
+      priority: 2,
+      silent: false,
     },
-)
+    () => {}
+  );
+});
 
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.time)
-            createAlarm();
-    }
-);
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.time) createAlarm();
+});
 
 function createAlarm() {
-    chrome.alarms.create(
-        "notifurcation",
-        {
-            delayInMinutes: 0,
-            periodInMinutes: 1 // THIS IS THE TIME (IN MINUTES) FOR EACH ALARM
-        }
-    );
+  chrome.alarms.create("notifurcation", {
+    delayInMinutes: 0,
+    periodInMinutes: 1, // THIS IS THE TIME (IN MINUTES) FOR EACH ALARM
+  });
 }
--- timer.js
+++ timer.js
@@ -1,49 +1,46 @@
 var counter = 0;
 var timeleft = 1800;
 
-function jsFunction(value)
-{
-    timeleft = value * 60;
+function jsFunction(value) {
+  counter = 0;
+  timeleft = value * 60;
 }
 
 function leftPad(number, targetLength) {
-    var output = number + '';
-    while (output.length < targetLength) {
-        output = '0' + output;
-    }
+  var output = number + "";
+  while (output.length < targetLength) {
+    output = "0" + output;
+  }
 
-    return output;
+  return output;
 }
 
 const countdown = () => {
-    
-    const countDate = new Date("July 17, 2022 00:00:00").getTime()
-    const now = new Date().getTime();
-    counter++;
-    const gap = timeleft - counter;
-    //const gap = countDate - now;
+  const countDate = new Date("July 17, 2022 00:00:00").getTime();
+  const now = new Date().getTime();
+  counter++;
+  const gap = timeleft - counter;
+  //const gap = countDate - now;
 
-    const  second = 1;
-    const  minute = second * 60;
-    const  hour = minute * 60;
-    const  day = hour * 24;
-    
+  const second = 1;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    const textDay = Math.floor(gap / day);
-    const textHour = Math.floor((gap % day)/ hour);
-    const textMinute = Math.floor((gap % hour)/ minute);
-    const textSecond = Math.floor((gap % minute)/ second);
+  const textDay = Math.floor(gap / day);
+  const textHour = Math.floor((gap % day) / hour);
+  const textMinute = Math.floor((gap % hour) / minute);
+  const textSecond = Math.floor((gap % minute) / second);
 
-    document.querySelector(".top-day").innerText = leftPad(textDay, 2);
-    document.querySelector(".top-hour").innerText = leftPad(textHour, 2);
-    document.querySelector(".top-minute").innerText = leftPad(textMinute, 2);
-    document.querySelector(".top-second").innerText = leftPad(textSecond, 2);
+  document.querySelector(".top-day").innerText = leftPad(textDay, 2);
+  document.querySelector(".top-hour").innerText = leftPad(textHour, 2);
+  document.querySelector(".top-minute").innerText = leftPad(textMinute, 2);
+  document.querySelector(".top-second").innerText = leftPad(textSecond, 2);
 
-    document.querySelector(".bottom-day").innerText = leftPad(textDay, 2);
-    document.querySelector(".bottom-hour").innerText = leftPad(textHour, 2);;
-    document.querySelector(".bottom-minute").innerText = leftPad(textMinute, 2);
-    document.querySelector(".bottom-second").innerText = leftPad(textSecond, 2);
-
+  document.querySelector(".bottom-day").innerText = leftPad(textDay, 2);
+  document.querySelector(".bottom-hour").innerText = leftPad(textHour, 2);
+  document.querySelector(".bottom-minute").innerText = leftPad(textMinute, 2);
+  document.querySelector(".bottom-second").innerText = leftPad(textSecond, 2);
 };
 
 setInterval(countdown, 1000);
